c7d477f4b3a82c9e05856dc655bbd22a5a572599
Quantexo <quantexo2005@gmail.com>
2025-05-21 19:57:36 +0545

    Add files via upload

--- Quantexo.py
+++ Quantexo.py
@@ -329,11 +329,11 @@ if company_symbol:
         fig.update_xaxes(
             rangeselector=dict(
                 buttons=list([
-                    dict(count=1, label="1m", step="month", stepmode="backward"),
                     dict(count=3, label="3m", step="month", stepmode="backward"),
                     dict(count=6, label="6m", step="month", stepmode="backward"),
                     dict(count=1, label="YTD", step="year", stepmode="todate"),
                     dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(count=2, label="2y", step="year", stepmode="backward"),
                     dict(step="all")
                 ])
             )
